750b05982c8b80bb155399b0d2b983fa06491357
Str4ch <dog.fek54@gmail.com>
2025-04-20 23:14:08 +0200

    use state added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useState } from "react";
 
 const HomePage = () => {
+    const [ product, setProduct ] = useState([])
     return (
         <div>
         <h1 className="text-center">App</h1>
+        <p>{!product.length && "no products yet."}</p>
         </div>
       )
 }
